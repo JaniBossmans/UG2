b917e061a3d25ec245f51cf125058fa61c64ed79
JaniBossmans <jani.bosmans@gmail.com>
2023-11-23 19:03:23 +0100

    Overgang Intro naar Level gemaakt
    Global Scene verwijderd omdat het onnodig is, overgang eerste 2 scenes doormiddel van button gedaan

--- UG2/Assets/Scripts/GameManager.cs
+++ UG2/Assets/Scripts/GameManager.cs
@@ -1,33 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance { get; private set; }
+
     private int level = 1;
     private int score = 0;
     private int lives = 3;
 
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);   
+        // Zorg ervoor dat er slechts één instantie van GameManager is
+        if (instance != null && instance != this)
+            Destroy(gameObject);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        
     }
+
     private void Start()
     {
         NewGame();
     }
+
     private void NewGame()
     {
         score = 0;
         lives = 3;
 
-        LoadLevel(1);
+        
     }
-    private void LoadLevel(int level)
-    {
-        this.level = level;
 
-        SceneManager.LoadScene("Level" + level);
+
+    public void ToLevel2Scene()
+    {
+        SceneManager.LoadScene("Level2");
     }
+
+    
+
+    public void ToScoreScene()
+    {
+        SceneManager.LoadScene("ScoreScene");
+    }
+
+    
 }
