d5af1e527da1e9c13c7852685554fd626ea4a22e
JaniBossmans <jani.bosmans@gmail.com>
2023-11-28 15:26:18 +0100

    bricks hebben nu health
    bricks hebben nu health

--- UG2/Assets/Scripts/GameManager.cs
+++ UG2/Assets/Scripts/GameManager.cs
@@ -3,20 +3,26 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance { get; private set; }
+    public static GameManager Instance { get; private set; }
+
+    private const int NUM_LEVELS = 2;
 
     private int level = 1;
     private int score = 0;
     private int lives = 3;
 
+    private Ball ball;
+    private Paddle paddle;
+    private Brick[] bricks;
+
     private void Awake()
     {
         // Zorg ervoor dat er slechts één instantie van GameManager is
-        if (instance != null && instance != this)
+        if (Instance != null && Instance != this)
             Destroy(gameObject);
         else
         {
-            instance = this;
+            Instance = this;
             DontDestroyOnLoad(gameObject);
         }
         
@@ -41,12 +47,47 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene("Level2");
     }
 
-    
-
     public void ToScoreScene()
     {
         SceneManager.LoadScene("ScoreScene");
     }
 
-    
+    public void OnBrickHit(Brick brick)
+    {
+        score += brick.points;
+
+        if (Cleared())
+        {
+            LoadLevel(level + 1);
+        }
+    }
+
+    private bool Cleared()
+    {
+        for (int i = 0; i < bricks.Length; i++)
+        {
+            if (bricks[i].gameObject.activeInHierarchy && !bricks[i].unbreakable)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void LoadLevel(int level)
+    {
+        this.level = level;
+
+        if (level > NUM_LEVELS)
+        {
+            // Start over again at level 1 once you have beaten all the levels
+            // You can also load a "Win" scene instead
+            LoadLevel(1);
+            return;
+        }
+
+        SceneManager.LoadScene("Level" + level);
+    }
+
 }
